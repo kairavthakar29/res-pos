cbd8ac16ef6578c5420bdff000aa40091e9cabd6
kairavthakar29 <kairav.thakar@achyutlabs.com>
2023-10-02 13:35:38 +0530

    commit

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -140,7 +140,7 @@ const filterProduct = newArray;
                                                         <i className="far fa-star text-secondary"></i> 
                                                         <i className="far fa-star text-secondary"></i> 
                                                     </div>
-                                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                                    {parse(<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>)}
                                                 </div>
                                                 {/* list END */}
                                             </li>
@@ -157,7 +157,7 @@ const filterProduct = newArray;
                                                         <i className="far fa-star text-secondary"></i> 
                                                         <i className="far fa-star text-secondary"></i> 
                                                     </div>
-                                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                                    {parse(<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>)}
                                                 </div>
                                             </li>
                                             <li className="comment">
@@ -173,7 +173,7 @@ const filterProduct = newArray;
                                                         <i className="fas fa-star text-secondary"></i> 
                                                         <i className="far fa-star text-secondary"></i> 
                                                     </div>
-                                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                                    {parse(<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>)}
                                                 </div>
                                             </li>
                                         </ul>
